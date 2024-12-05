d8fec16f2727fa37706f80c99da3df5e6a91d7cd
deamates2 <mthsfeltrin@gmail.com>
2024-12-05 11:57:44 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -17,7 +17,25 @@ criaCartao(
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Educação fisica',
+    'Quantos ossos tem no corpo humano?',
+    '206 ossos'
+)
+
+criaCartao(
+    'Historia',
+    'Quando foi a guerra de canudos?',
+    '7 de novembro de 1896 ate 5 de outubro de 1897'
+)
+
+    criaCartao(
+    'Espanhol',
+    'Como se escreve sobrenome em espanhol?',
+    'Apellido'
 )
